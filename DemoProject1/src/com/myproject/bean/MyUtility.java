19ba8a253f0dcd4a8a3a5b1f11c0bf62ddf0cf46
anand980980 <anand980980@gmail.com>
2021-12-07 15:46:05 +0530

    modified sysout by team lead

--- DemoProject1/src/com/myproject/bean/MyUtility.java
+++ DemoProject1/src/com/myproject/bean/MyUtility.java
@@ -5,7 +5,7 @@ public class MyUtility {
 	public static void main(String[] args)
 	{
 
-	System.out.println("hi the changes are done");
+	System.out.println("hi the changes are done ...123");
 	}
 	
 }
